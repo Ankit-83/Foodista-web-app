7697130b4cdc501b91a4675a51f7b496deee0e09
Ankit Prajapati <65863084+Ankit-83@users.noreply.github.com>
2025-03-20 05:25:11 +0000

    fix issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,6 @@ import Cart from '../components/Cart'
 const Home = () => {
   return (
     <>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-B6NX6K8FLJ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
-
-  gtag('config', 'G-B6NX6K8FLJ');
-</script>
     <Navbar />
     <CategoryMenu />
     <FoodItem />
